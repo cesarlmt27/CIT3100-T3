6625edfc49e0b1015d56fb817b6009aecb6508ad
César Montero <72035439+cesarlmt27@users.noreply.github.com>
2024-07-13 05:31:59 -0400

    No retornar el password

--- iot_platform/api/serializers.py
+++ iot_platform/api/serializers.py
@@ -6,20 +6,32 @@ class AdminSerializer(serializers.ModelSerializer):
 		model = Admin
 		fields = '__all__'
 
+	def to_representation(self, instance):
+		# Llamar al método to_representation del padre para obtener el diccionario original
+		ret = super().to_representation(instance)
+		# Eliminar el campo "password" del diccionario
+		ret.pop('password', None)
+		# Retornar el diccionario modificado
+		return ret
+
+
 class CompanySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Company
 		fields = '__all__'
+
 
 class LocationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Location
 		fields = '__all__'
 
+
 class SensorSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Sensor
 		fields = '__all__'
+
 
 class SensorDataSerializer(serializers.ModelSerializer):
 	class Meta:
